93bee15b5d3735f7f6d0252f968729f89e7e55c8
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-10-04 16:33:10 -0300

    nullable and dynamic types

---
+++ AdvancedCSCourse/DynamicTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DynamicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# is a static language, such as Java
+            //A dynamic language is referred to Ruby, Python or Javascript
+            //The difference is that static langauges are ran at compile time, and dynamic languages are ran at run time
+            //it's faster to write code
+
+            dynamic name = "Gabriel";
+            //name++; => exception, with dynamic we need to write more unit tests
+            name = 10; //no problems on doing that
+
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+
+            System.Console.WriteLine(l);
+        }
+    }
+}
---
+++ AdvancedCSCourse/NullableTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        public static void Main (string[] args) {
+            //DateTime date = null; => would throw an error
+            //Nullable<DateTime> date = null; => correct syntax
+            DateTime? date = null; //shorthand
+
+            Console.WriteLine("GetValueOrDefault(): " + date.GetValueOrDefault()); //returns a value if the object is initialized or the data type default if it's not
+            Console.WriteLine("HasValue: " + date.HasValue); //true if it has a value, false if it's null
+            //Console.WriteLine("Value: " + date.Value);  =>  must have a value or the app crashes, GetValueOrDefault is the preferred way
+
+            date = new DateTime(2021, 10, 04);
+            //DateTime date2 = date;  =>  error, because is trying to give a nullable DateTime to a normal one, what if it's null, compiler doesn't know what to do
+            DateTime date2 = date.GetValueOrDefault(); //right way
+            Console.WriteLine(date2);
+
+            //DateTime date3 = (Date != null) ? date.GetValueOrDefault() : DateTime.Today;
+            DateTime date3 = date ?? DateTime.Today; //null coalescing operator
+            //good use instead of if and else (if date has a value, will be date, else, DateTime.Today)
+        }
+    }
+}
