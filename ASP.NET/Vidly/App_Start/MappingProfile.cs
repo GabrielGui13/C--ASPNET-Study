3b4917c62f333b7cc84680aec52b0a137681cc98
Gabriel Guilherme <gabrielguilherme13@hotmail.com>
2022-01-03 00:33:23 -0300

    mapeamento da api

---
+++ ASP.NET/Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using Vidly.Models;
+using Vidly.Dtos;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
--- ASP.NET/Vidly/Controllers/Api/CustomersAPIController.cs
+++ ASP.NET/Vidly/Controllers/Api/CustomersAPIController.cs
@@ -9,14 +9,22 @@ using System.Net.Http;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Vidly.Models;
+using Vidly.Dtos;
 using Vidly.ViewModels;
 using Vidly.Context;
+using AutoMapper;
 
 //controller normal
 //baixar as dependencias webhost e webapi
-//precisa inicializar a config de web api em Global.asax
 //um arquivo webapiconfig.cs precisa ser criado em App_Start
+//precisa inicializar a config de web api em Global.asax
 //dependencias importantes => System.Net; System.Net.Http; System.Web; System.Web.Http; System.Web.Optimization; System.Web.Routing;
+
+//automapper
+// baixar a dependencia automapper -version:4.1.0
+//criar o arquivo mappingprofile no app_start e criar os mapeamentos
+//inicializar o profile no Global.asax
+//apos isso pode usar normal
 
 namespace Vidly.Controllers
 {
@@ -31,37 +39,40 @@ namespace Vidly.Controllers
         }
 
         // GET api/customers
-        public IEnumerable<Customer> GetCustomers()
+        public IEnumerable<CustomerDto> GetCustomers()
         {
-            return db.Customers.ToList();
+            return db.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
         }
 
         // GET /api/customers/1
-        public Customer GetCustomer(int id)
+        public CustomerDto GetCustomer(int id)
         {
             var customer = db.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null) throw new HttpResponseException(HttpStatusCode.NotFound); //HttpStatusCode derives from System.Net;
 
-            return customer;
+            return Mapper.Map<Customer, CustomerDto>(customer);
         }
 
         // POST api/customers
         [HttpPost] //CustomersController => System.Web.Mvc / Here => System.Web.Http
-        public Customer CreateCustomer (Customer customer)
+        public CustomerDto CreateCustomer (CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             db.Customers.Add(customer);
             db.SaveChanges();
 
-            return customer;
+            customerDto.Id = customer.Id;
+
+            return customerDto;
         }
 
         // PUT /api/customers/1
         [HttpPut]
-        public void UpdateCustomer(int id, Customer customer) //id from url and customer from req body
+        public void UpdateCustomer(int id, CustomerDto customerDto) //id from url and customer from req body
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
@@ -71,10 +82,7 @@ namespace Vidly.Controllers
             if (customerInDb == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
-            customerInDb.Name = customer.Name;
-            customerInDb.Birthdate = customer.Birthdate;
-            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
-            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            Mapper.Map(customerDto, customerInDb);
 
             db.SaveChanges();
         }
